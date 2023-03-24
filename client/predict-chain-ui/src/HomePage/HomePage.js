e59de6ab1571f52a618c11b7f760287e5263e568
connorpatterson <connor.internship@pipesearch.com>
2023-03-24 12:53:28 -0400

    added Front End "Hello World"

---
+++ client/predict-chain-ui/src/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ client/predict-chain-ui/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HomePage from './HomePage/HomePage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <HomePage/>
+  </React.StrictMode>
+);
+
+// ADD COMPONENTS FOR RESPECTIVE PAGES/PARTS OF WEBPAGE

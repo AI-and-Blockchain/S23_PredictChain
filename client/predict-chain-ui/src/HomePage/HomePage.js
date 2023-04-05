068340012739a874e83eb9bde0c38316c728574a
connorpatterson <connor.internship@pipesearch.com>
2023-04-05 14:42:08 -0400

    More QoL changes

--- client/predict-chain-ui/src/HomePage/HomePage.js
+++ client/predict-chain-ui/src/HomePage/HomePage.js
@@ -13,13 +13,13 @@ function HomePage() {
             <img src={require('../img/prediction.png')} style={{width: '400px', height: '400px'}}/>
           </div>
           <div className="text" style={{marginLeft: '100px', marginRight: '200px', marginTop: '50px'}}>
-            <h1>What is PredictChain?</h1>
+            <h1 style={{textDecoration: "underline"}}>What is PredictChain?</h1>
             <h4 style={{fontSize: "30px", fontWeight: "lighter"}}>PredictChain is a marketplace for predictive AI models. Users will be able to upload datasets to train predictive models, or submit queries to those models. These various models will be operated by a central node or nodes with computing resources available. A variety of models will be made available, ranging from cheap, fast, and simple to more expensive, slower, and more powerful. This will allow for a large variety of predictive abilities for both simple and complex patterns. All the past predictions form these models will be stored on the blockchain for public viewing.</h4>
           </div>
         </div>
         <div className="containertext">
           <div className="text" style={{marginLeft: '200px', marginRight: '100px', marginTop: '50px'}}>
-            <h1>How do we differ from our competitors?</h1>
+            <h1 style={{textDecoration: "underline"}}>How do we differ from our competitors?</h1>
             <h4 style={{fontSize: "30px", fontWeight: "lighter"}}>Our motivation is to fill a similar niche to marketplace DApps, but will accommodate to the user more. We are working towards a more varied experience when it comes to decentralized ML platforms.  Over the course of the month, we plan to improve upon this idea to provide users with more flexibility in their use cases.</h4>
           </div>
           <div className="img" style={{marginRight: '200px', marginTop: '50px'}}>
@@ -32,8 +32,8 @@ function HomePage() {
         <br/>
         <br/>
         <div className="text" style={{marginTop: '50px'}}>
-          <h1 style={{textAlign: "center"}}>Who are we?</h1>
-          <h4 style={{fontSize: "30px", fontWeight: "lighter", textAlign: "center"}}>Just a group of <a href="https://www.rpi.edu" className="temp">RPI</a> students in an AI & Blockchain class. This is our group project for the Spring 2023 semester. More in-depth detail about us and who we are located in our <a href="/mtt" className="temp">Meet The Team</a> page!</h4>
+          <h1 style={{textAlign: "center", textDecoration: "underline"}}>Who are we?</h1>
+          <h4 style={{marginLeft: "255px", marginRight: "255px", fontSize: "30px", fontWeight: "lighter", textAlign: "center"}}>Just a group of <a href="https://www.rpi.edu" className="temp">RPI</a> students in an AI & Blockchain class. This is our group project for the Spring 2023 semester. More in-depth detail about us and who we are located in our <a href="/mtt" className="temp">Meet The Team</a> page!</h4>
         </div>
         <br/>
         <Footer/>
--- client/predict-chain-ui/src/Slideshow/Slideshow.js
+++ client/predict-chain-ui/src/Slideshow/Slideshow.js
@@ -3,7 +3,7 @@ function Slideshow() {
   return (
     <div>
         <div className="image-gallery-container">
-            <h1 style={{textAlign: "center"}}>Example Models We Provide</h1>
+            <h1 style={{textAlign: "center",  textDecoration: "underline"}}>Example Models We Provide</h1>
             <img src={require('../img/decision-tree.png')} style={{marginLeft: "50px", border: "4px solid black", borderRadius: "3px"}} alt="Decision Trees"/>
             <img src={require('../img/perceptrons.png')} style={{marginLeft: "25px", marginRight: "25px", border: "4px solid black", borderRadius: "3px"}} alt="Perceptrons"/>
             <img src={require('../img/nn.png')} style={{width: "600px", border: "4px solid black", borderRadius: "3px"}} alt="Neural Networks"/>

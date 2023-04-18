6b0ca04719f6e4f3b1b25920a8a960045538921b
matthew-pisano <matthewpisano14@gmail.com>
2023-04-18 09:42:40 -0400

    updateReport

---
+++ conf.js
@@ -0,0 +1,21 @@
+'use scrict';
+
+
+module.exports = {
+    "plugins": [],
+    "recurseDepth": 10,
+    "source": {
+        "includePattern": ".+\\.js(doc|x)?$",
+        "excludePattern": "(^|\\/|\\\\)_"
+    },
+    "sourceType": "module",
+    "tags": {
+        "allowUnknownTags": true,
+        "dictionaries": ["jsdoc","closure"]
+    },
+    "templates": {
+        "cleverLinks": false,
+        "monospaceLinks": false
+    }
+};
+
